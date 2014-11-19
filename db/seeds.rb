a9e9b49209decc136e67d95e537e5703185a58c9
James Duerden <9c1c33c8@opayq.com>
2014-11-19 21:54:14 +0000

    price fixing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+home_list = [
+  [ "One Bedroom", 69, 99, 129],
+  [ "Two Bedrooms", 79, 109, 139],
+  [ "Three Bedrooms", 89, 119, 149],
+  [ "Four Bedrooms", 99, 129, 159],
+  [ "Five Bedrooms", 109, 139, 169]
+]
+
+home_list.each do |size, bronze_price, silver_price, gold_price|
+  Home.create( size: size, bronze_price: bronze_price, silver_price: silver_price, gold_price: gold_price )
+end
