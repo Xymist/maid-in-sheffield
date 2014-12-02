937fa05fbe806895ddfa5fe4d8796f2ffa39d21b
James Duerden <9c1c33c8@opayq.com>
2014-12-02 14:05:53 +0000

    betterconformat

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,8 @@
 class SiteController < ApplicationController
   def price_grid
     @prices = Home.all.order('id')
-    @services = Service.all.order('id')
+    @services = Service.all.order('number')
+    @firstservice = Service.first
+    @lastservice = Service.last
   end
 end
