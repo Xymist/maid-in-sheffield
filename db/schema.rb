1e88d440801f2daaacd16e07c466c2f8fe29ec5b
James Duerden <jamie.f.duerden@gmail.com>
2015-02-25 02:19:04 +0000

    this

--- db/schema.rb
+++ db/schema.rb
@@ -35,10 +35,10 @@ ActiveRecord::Schema.define(version: 20150102185238) do
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "cvs", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.string   "cv_uid"
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "homes", force: true do |t|
@@ -78,9 +78,6 @@ ActiveRecord::Schema.define(version: 20150102185238) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "cvs"
-    t.string   "name"
-    t.string   "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
